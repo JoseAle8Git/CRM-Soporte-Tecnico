30743abd3d981571e483d98a9c09fe4892adaafa
Jose Alejandro Rodriguez Gil <josealejrg@gmail.com>
2025-11-03 22:25:35 +0100

    Creación del auth y cambios para redireccionar la web al inicio al login, y también cambios en el archivo auth.ts para crear la lógica que hará la llamada HTTP al backend.

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,25 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
 
-export const routes: Routes = [];
+// Definición de las rutas principales.
+const routes: Routes = [
+    // 1. Rita de autenticación. (Carga perezosa)
+    // Cuando el usuario va a 'localhost:4200/auth', carga el modulo Auth.
+    {
+        path: 'auth',
+        loadChildren: () => import('./auth/auth-module').then(m => m.AuthModule)
+    },
+    // 2. Ruta por defecto/raíz.
+    // Redirige la URL raíz ('/') a la ruta de login.
+    {
+        path: '',
+        redirectTo: 'auth/login',
+        pathMatch: 'full'
+    }
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+export class AppRoutingModule {}
