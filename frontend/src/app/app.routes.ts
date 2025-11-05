c3bf31e6d9f241de4a6f36f8809bd7400da2a164
Jose Alejandro Rodriguez Gil <josealejrg@gmail.com>
2025-11-05 19:51:05 +0100

    Se ha vuelto a contruir todo el frontend de cero y no se han añadido más cambios. En la parte del backend se han añadido configuraciones en el archivo application.properties.

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,25 +1,3 @@
-import { RouterModule, Routes } from '@angular/router';
-import { NgModule } from '@angular/core';
+import { Routes } from '@angular/router';
 
-// Definición de las rutas principales.
-const routes: Routes = [
-    // 1. Rita de autenticación. (Carga perezosa)
-    // Cuando el usuario va a 'localhost:4200/auth', carga el modulo Auth.
-    {
-        path: 'auth',
-        loadChildren: () => import('./auth/auth-module').then(m => m.AuthModule)
-    },
-    // 2. Ruta por defecto/raíz.
-    // Redirige la URL raíz ('/') a la ruta de login.
-    {
-        path: '',
-        redirectTo: 'auth/login',
-        pathMatch: 'full'
-    }
-];
-
-@NgModule({
-    imports: [RouterModule.forRoot(routes)],
-    exports: [RouterModule]
-})
-export class AppRoutingModule {}
+export const routes: Routes = [];
